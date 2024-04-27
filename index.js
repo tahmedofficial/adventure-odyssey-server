06e0ed24969628152beb77f62ef23fb1c0d72f02
tahmedofficial <tanvirofcuo@gmail.com>
2024-04-28 00:42:25 +0600

    fixed some error

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// { origin: ["localhost url", "live url"] }
+app.use(cors({ origin: ["localhost url", "live url"] }))
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,6 +28,7 @@ async function run() {
 
         const database = client.db("tourismDB")
         const tourismCollection = database.collection("tourism")
+        const countryesCollection = database.collection("countryes")
 
         app.get("/travel", async (req, res) => {
             const cursor = tourismCollection.find();
@@ -77,6 +79,20 @@ async function run() {
             res.send(result);
         })
 
+        // Country related api
+
+        app.get("/country", async (req, res) => {
+            const cursor = countryesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post("/country", async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const result = await countryesCollection.insertOne(data);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
