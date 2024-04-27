0e9c2f1ac1f9c66d1c7ea54500c1d2f1ce70a653
tahmedofficial <tanvirofcuo@gmail.com>
2024-04-27 12:21:29 +0600

    added data post and get function

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: ["localhost url", "live url"] }));
+app.use(cors());
 app.use(express.json());
-
-
-
-// adventureOdyssey 
-// Jvii9Jyn8lMx6vd7
-
+// { origin: ["localhost url", "live url"] }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufkobjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,8 +25,21 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const database=client.db("tourismDB")
-        const tourismCollection=database.collection("tourism")
+        const database = client.db("tourismDB")
+        const tourismCollection = database.collection("tourism")
+
+        app.get("/travel", async (req, res) => {
+            const cursor = tourismCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post("/travel", async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const result = await tourismCollection.insertOne(data);
+            res.send(result);
+        })
 
 
 
